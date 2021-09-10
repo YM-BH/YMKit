8b6a080e3835824e275a25e7f2281c9ff7f5f54d
LazyLoad <baih_1989@163.com>
2021-09-10 14:46:03 +0800

    集成pod
    日志demo 做准备

---
+++ YMKit/YMKit/Utils/Log/LogUtil.h
@@ -0,0 +1,17 @@
+//
+//  LogUtil.h
+//  YMKit
+//
+//  Created by 白晗 on 2021/9/10.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// 日志工具类
+@interface LogUtil : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YMKit/YMKit/Utils/Log/LogUtil.m
@@ -0,0 +1,12 @@
+//
+//  LogUtil.m
+//  YMKit
+//
+//  Created by 白晗 on 2021/9/10.
+//
+
+#import "LogUtil.h"
+
+@implementation LogUtil
+
+@end
