e7540b6be36c5a5d76368ec13bcf21a7a44bd97d
LazyLoad <baih_1989@163.com>
2021-09-16 10:55:03 +0800

    图片置灰

---
+++ YMKit/YMKit/Categories/UIImage+YMExtension.h
@@ -0,0 +1,19 @@
+//
+//  UIImage+YMExtension.h
+//  YMKit
+//
+//  Created by 白晗 on 2021/9/16.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIImage (YMExtension)
+
+/// 将图片置灰
+- (UIImage *)ym_grayImage:(UIImage *)image;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YMKit/YMKit/Categories/UIImage+YMExtension.m
@@ -0,0 +1,34 @@
+//
+//  UIImage+YMExtension.m
+//  YMKit
+//
+//  Created by 白晗 on 2021/9/16.
+//
+
+#import "UIImage+YMExtension.h"
+
+@implementation UIImage (YMExtension)
+
+- (UIImage *)ym_grayImage:(UIImage *)image {
+    int bitmapInfo = kCGImageAlphaNone;
+    int width = image.size.width;
+    int height = image.size.height;
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceGray();
+    
+    CGContextRef context = CGBitmapContextCreate(nil, width, height, 8, 0, colorSpace, bitmapInfo);
+
+    CGColorSpaceRelease(colorSpace);
+    if (context == NULL) {
+        return nil;
+    }
+    CGContextDrawImage(context, CGRectMake(0, 0, width, height), image.CGImage);
+    
+    UIImage *grayImage = [UIImage imageWithCGImage:CGBitmapContextCreateImage(context)];
+
+    CGContextRelease(context);
+    return grayImage;
+
+}
+
+@end
