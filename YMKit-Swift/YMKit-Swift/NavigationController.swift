95c7e01cf706223b9b8538640c5b280226f9be76
LazyLoad <baih_1989@163.com>
2021-09-17 14:31:03 +0800

    拥抱Swift

---
+++ YMKit-Swift/YMKit-Swift/NavigationController.swift
@@ -0,0 +1,29 @@
+//
+//  NavigationController.swift
+//  YMKit-Swift
+//
+//  Created by 白晗 on 2021/9/17.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ YMKit-Swift/YMKit-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YMKit-Swift
+//
+//  Created by 白晗 on 2021/9/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
